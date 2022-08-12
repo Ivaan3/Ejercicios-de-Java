44acc8e1471c5fc224ab0d431d3416070e778d34
Ivaan3 <106582115+Ivaan3@users.noreply.github.com>
2022-08-12 11:37:15 +0200

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,15 @@
+const lista =  new Array (5);
+
+lista[0] = "Ivan Eduardo Alvaretto";
+lista[1] = 30;
+lista[2] = false;
+lista[3] = new Date(1991, 08, 27);
+lista[4] = {
+    titulo: "El Alquimista",
+    autor: "Paulo Coelho",
+    fecha: 1988,
+    url: "https://books.google.dk/books/about/El_Alquimista.html?id=lZZCzTM_9PUC&printsec=frontcover&source=kp_read_button&hl=es&redir_esc=y#v=onepage&q&f=false"
+};
+
+
+console.log(lista);
